10731b70b4fefcbf52bc98bd1002bc44fae667b0
zeynepturnali <zeynepturnali23@gmail.com>
2020-09-10 16:48:32 +0300

    finished

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,11 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var firstInput: UITextField!
+    @IBOutlet var secondInput: UITextField!
+    @IBOutlet var result: UILabel!
+    var addResult = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func addButton(_ sender: Any) {
+        if let firstNumber = Int (firstInput.text!){
+            if let secondNumber = Int (secondInput.text!){
+                addResult = firstNumber + secondNumber
+                result.text = String (addResult)
+            }
+        }
+    
+    }
+    
+    @IBAction func substractButton(_ sender: Any) {
+        if let firstNumber = Int (firstInput.text!){
+            if let secondNumber = Int (secondInput.text!){
+                addResult = firstNumber - secondNumber
+                result.text = String (addResult)
+            }
+        }
+        
+    }
+    @IBAction func productButton(_ sender: Any) {
+        if let firstNumber = Int (firstInput.text!){
+            if let secondNumber = Int (secondInput.text!){
+                addResult = firstNumber * secondNumber
+                result.text = String (addResult)
+            }
+        }
+        
+        
+    }
+    @IBAction func devideButton(_ sender: Any) {
+        if let firstNumber = Int (firstInput.text!){
+            if let secondNumber = Int (secondInput.text!){
+                addResult = firstNumber / secondNumber
+                result.text = String (addResult)
+            }
+        }
+        
+        
+    }
+    
 }
 
