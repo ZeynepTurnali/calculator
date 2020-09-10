b42bd0e04f1a93af25022b05382424045f143ba5
zeynepturnali <zeynepturnali23@gmail.com>
2020-09-10 14:02:45 +0300

    Initial Commit

---
+++ calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by testinium on 10.09.2020.
+//  Copyright © 2020 testinium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
